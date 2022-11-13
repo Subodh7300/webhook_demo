453aa358abaa6288109a4542dd28d03c8f46adc9
Subodh7300 <97154559+Subodh7300@users.noreply.github.com>
2022-11-13 21:20:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def webhook():
     try:
         if request.headers['key'] == "UZw3KfpfGU2QpMFEOWhQNSmnXyhOGD6G" and request.headers['Content-Type'] == "application/json":
-            return {"success": True}, 200, "OK"
+            return {"success": True}, 200
         else:
-            return {"success": False}, 400, "Bad request"
+            return {"success": False}, 400
     except:
-        return {"success": False}, 500, "Authentication error"
+        return {"success": False}, 500
 
 
 if __name__ == "__main__":
